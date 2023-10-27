d808eb667e3c696f63721e60e00b272af275c711
hidelgionovela <hidelgionovela@gmail.com>
2023-10-27 23:03:12 +0200

    chore: adding findByCreditCode function in CreditRepository

--- src/main/kotlin/hdlg/la/credit/application/system/repository/CreditRepository.kt
+++ src/main/kotlin/hdlg/la/credit/application/system/repository/CreditRepository.kt
@@ -3,7 +3,9 @@ package hdlg.la.credit.application.system.repository
 import hdlg.la.credit.application.system.entity.Credit
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.util.UUID
 
 @Repository
 interface CreditRepository: JpaRepository<Credit, Long> {
+    fun findByCreditCode(creditCode: UUID): Credit?
 }
