ade88e5601a5562327a5093272908b82b044043c
hidelgionovela <hidelgionovela@gmail.com>
2023-11-14 22:46:08 +0200

    Create CreditView.kt

---
+++ src/main/kotlin/hdlg/la/credit/application/system/dto/response/CreditView.kt
@@ -0,0 +1,22 @@
+package hdlg.la.credit.application.system.dto.response
+
+import hdlg.la.credit.application.system.entity.Credit
+import hdlg.la.credit.application.system.enums.Status
+import java.math.BigDecimal
+import java.util.*
+
+data class CreditView(
+    val creditCode: UUID,
+    val creditValue: BigDecimal,
+    val numberOfInstallment: Int,
+    val status: Status,
+    val emailCustomer: String?
+) {
+    constructor(credit: Credit) : this(
+        creditCode = credit.creditCode,
+        creditValue = credit.creditValue,
+        numberOfInstallment = credit.numberOfInstallments,
+        status = credit.status,
+        emailCustomer = credit.customer?.email
+    )
+}
